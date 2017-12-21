ea26f22baa84e0434a35035ec523f172d3bf12c3
Nick Vitebsky <n.vitebsky1327@gmail.com>
2017-12-21 16:54:14 -0500

    first commit

---
+++ simple.c
@@ -0,0 +1,8 @@
+//https://msdn.microsoft.com/en-us/library/bb384838.aspx
+#include <stdio.h>  
+
+int main()  
+{  
+    printf("Hello, World! This is a native C program compiled on the command line.\n");  
+    return 0;  
+}  
